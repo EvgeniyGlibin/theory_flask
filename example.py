4c3e877ca1a18b6d64c5e9ffed4fed79d6494a81
EvgeniyGlibin <evgeniyglibin95@gmail.com>
2023-10-16 06:16:38 +0900

    create a built-in wsgi server

--- example.py
+++ example.py
@@ -1,7 +1,10 @@
+from datetime import datetime
+
 def app(environ, start_responce):
-    data = b"Hello, World!\n"
+    time = datetime.now()
+    data = bytes(f'The time is {time:%b %d %H:%M:%S}', 'utf-8')
     start_responce("200 OK", [
         ("Content-Type", "text/plain"),
         ("Content-Length", str(len(data)))
     ])
-    return iter([data])
+    return [data]
